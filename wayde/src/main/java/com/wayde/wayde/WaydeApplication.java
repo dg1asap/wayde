7c2af65295b1e1d79f85936d0419673e9e28559a
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2024-07-11 20:12:03 +0200

    init

---
+++ wayde/src/main/java/com/wayde/wayde/WaydeApplication.java
@@ -0,0 +1,13 @@
+package com.wayde.wayde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaydeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaydeApplication.class, args);
+    }
+
+}
---
+++ wayde/src/test/java/com/wayde/wayde/WaydeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayde.wayde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaydeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
